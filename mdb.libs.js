63b7c5b0919746ed6ddfa1007ec2b19e5a27296e
RistoReborn91 <48331300+RistoReborn91@users.noreply.github.com>
2023-09-04 01:33:35 -0400

    Create SetLinkedStatus()

--- mdb.libs.js
+++ mdb.libs.js
@@ -1,7 +1,5 @@
 function GetEntriesLinkedTo(e, lib) {
-
   return libByName(lib).linksTo(e);
-
 }
 
 function CountEntriesLinkedTo(e,lib) {
@@ -10,4 +8,12 @@ function CountEntriesLinkedTo(e,lib) {
 
 function HasEntriesLinkedTo(e, lib) {
   return CountEntriesLinkedTo(e,lib) > 0;
+}
+
+function SetLinkedStatus(e, lib, f, status1, status2) {
+
+  if (HasEntriesLinkedTo(e, lib))
+    e.set(f, status1);
+  else
+    e.set(f, status2);
 }
